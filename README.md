685ae8b3bf6d06b045a7337d63a71ea5b31b187c
rakarmp <raka_088@smkassalaambandung.sch.id>
2023-08-06 22:48:18 +0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,29 @@
+<img src="pyquote.png" alt="pyquote"/>
+
+# Pyquote
+
+REST API Pyquote adalah sebuah Rest API (Application Programming Interface) web yang menyediakan layanan untuk mengelola dan mengakses kumpulan quote. Rest API ini dibangun menggunakan Python FastAPI dan terhubung dengan MongoDB sebagai database.
+
+## Endpoint yang Tersedia
+
+-   GET /quotes: Mengembalikan daftar quote dalam bentuk JSON.
+-   GET /quotes/{quote_id}: Mengembalikan quote dengan ID yang sesuai dalam bentuk JSON.
+-   POST /quotes: Membuat quote baru dan menyimpannya ke dalam koleksi  quotes.
+-   PUT /quotes/{quote_id}: Memperbarui quote dengan ID tertentu dalam koleksi  quotes.
+-   DELETE /quotes/{quote_id}: Menghapus quote dengan ID tertentu dari koleksi  quotes.
+
+## Format Data Quote
+
+Setiap quote memiliki atribut berikut:
+
+-   id: ID unik untuk quote.
+-   text: Teks dari quote.
+-   author: Penulis atau sumber dari quote.
+
+## Penggunaan
+
+Anda dapat menggunakan aplikasi ini dengan mengirimkan permintaan HTTP ke endpoint yang sesuai menggunakan metode GET, POST, PUT, atau DELETE. Permintaan harus memiliki header Content-Type: application/json dan body permintaan harus berisi data quote dalam format JSON.
+
+Pastikan Anda telah menjalankan server FastAPI dan MongoDB sebelum menggunakan REST API ini. Pastikan juga bahwa koleksi quotes telah dibuat di dalam database MongoDB yang terhubung.
+
+Selamat menggunakan REST API Pyquote!
---
+++ main.py
@@ -0,0 +1,66 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from pymongo import MongoClient
+from typing import List
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# Konfigurasi CORS
+origins = [
+    "http://localhost:8000",
+    "http://localhost:8000/quotes"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"]
+)
+
+# Koneksi ke MongoDB
+client = MongoClient("YOUR MONGODB URL")
+db = client["quote_db"]
+collection = db["quotes"]
+
+class Quote(BaseModel):
+    id: str
+    text: str
+    author: str
+
+@app.get("/quotes", response_model=List[Quote])
+def get_quotes():
+    quotes = list(collection.find())
+    return quotes
+
+@app.get("/quotes/{quote_id}", response_model=Quote)
+def get_quote(quote_id: str):
+    quote = collection.find_one({"id": quote_id})
+    if quote:
+        return quote
+    return {"error": "Quote not found"}
+
+@app.post("/quotes", response_model=Quote)
+def create_quote(quote: Quote):
+    collection.insert_one(quote.dict())
+    return quote
+
+@app.put("/quotes/{quote_id}", response_model=Quote)
+def update_quote(quote_id: str, quote: Quote):
+    updated_quote = collection.find_one_and_update(
+        {"id": quote_id},
+        {"$set": quote.dict()},
+        return_document=True
+    )
+    if updated_quote:
+        return updated_quote
+    return {"error": "Quote not found"}
+
+@app.delete("/quotes/{quote_id}")
+def delete_quote(quote_id: str):
+    result = collection.delete_one({"id": quote_id})
+    if result.deleted_count > 0:
+        return {"message": "Quote deleted"}
+    return {"error": "Quote not found"}
